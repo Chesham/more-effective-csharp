77a83f90bb6d11459675c795abe0cda2f1291c59
cheshamchou <cheshamchou@moneydj.com>
2020-03-03 23:22:45 +0800

    item 02 slide and project

---
+++ more-effective-csharp-2nd-edition/sources/item02/Item02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace item02
+{
+    [Serializable]
+    public class Item02 : IEquatable<Item02>
+    {
+        public string Name { get; set; }
+
+        public bool Equals([AllowNull] Item02 other) => Name == other.Name;
+
+        public override bool Equals(object obj) => (obj as Item02)?.Equals(this) ?? false;
+
+        public override int GetHashCode() => Name.GetHashCode();
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item02.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass]
+    public class Item02
+    {
+        [TestMethod]
+        public void TestSerializedInMemory()
+        {
+            var expect = new item02.Item02 { Name = "Hello" };
+            var serializer = new BinaryFormatter();
+            var serialized = default(byte[]);
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, expect);
+                serialized = stream.ToArray();
+            }
+            var actual = default(item02.Item02);
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(serialized, 0, serialized.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                actual = (item02.Item02)serializer.Deserialize(stream);
+            }
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
