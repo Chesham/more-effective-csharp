d345726d20eddca20f8403196f026dacd9fb7c88
paigekuo <paigekuo@moneydj.com>
2020-03-06 15:59:17 +0800

    item 03 slide and project

---
+++ more-effective-csharp-2nd-edition/sources/Itme03/Item03.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+
+namespace item03
+{
+    public struct Item03
+    {
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public string City { get; }
+
+        public string[] Phones { get; }
+
+        public IEnumerable<string> Phones2 { get; }
+
+        public Item03(string name,int age, string city,string[] phones) {
+            Name = name;
+            City = city;
+            Age = age;
+            Phones = phones;
+            Phones2 = phones.ToImmutableList();
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item03.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using item03;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class Item03
+    {
+        [TestMethod()]
+        public void TestItem03()
+        {
+            var ph = new string[10];
+            var target = new item03.Item03("Paigekuo",25,"Kaohsiung",ph);
+            var expect = 25;
+            var actual = target.Age;
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void TestArrayValue()
+        {
+            var ph = new string[10];
+            ph[0] = "09XX-XXX-XXX";
+            var target = new item03.Item03("Paigekuo", 25, "Kaohsiung", ph);
+            ph[0] = "09OOO-OOO-OOO";
+            var expect = "09XX-XXX-XXX";
+            var actual = target.Phones2.First();
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void TestArrayRefrernce()
+        {
+            var ph = new string[10];
+            ph[0] = "09XX-XXX-XXX";
+            var target = new item03.Item03("Paigekuo", 25, "Kaohsiung", ph);
+            ph[0] = "09OOO-OOO-OOO";
+            var expect = "09XX-XXX-XXX";
+            var actual = target.Phones.First();
+            Assert.AreNotEqual(expect, actual);
+        }
+
+    }
+}
