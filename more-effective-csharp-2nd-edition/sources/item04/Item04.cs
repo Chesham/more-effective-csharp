11b03c17feccb8fdb06f832d0129dcde7567a8ec
paigekuo <paigekuo@moneydj.com>
2020-03-09 14:37:59 +0800

    item 04 slide and project

---
+++ more-effective-csharp-2nd-edition/sources/item04/Item04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace item04
+{
+    public struct EmployeebyStruct
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public EmployeebyStruct(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+
+    public class EmployeebyClass
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public EmployeebyClass(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item04.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using item04;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class Item04
+    {
+        [TestMethod()]
+        public void TestStructType()
+        {
+            List<EmployeebyStruct> Employees = new List<EmployeebyStruct>();
+            EmployeebyStruct e = new EmployeebyStruct("CEO", 50000);
+            Employees.Add(e);
+
+            var current = Employees.Find(ee => ee.Name == "CEO");
+            int Bonus = 10000;
+            current.Salary += Bonus;
+
+            var expect = 50000;
+            var actual = Employees.Find(ee => ee.Name == "CEO").Salary;
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void TestClassType()
+        {
+            List<EmployeebyClass> Employees = new List<EmployeebyClass>();
+            EmployeebyClass e = new EmployeebyClass("CEO", 50000);
+            Employees.Add(e);
+
+            var current = Employees.Find(ee => ee.Name == "CEO");
+            int Bonus = 10000;
+            current.Salary += Bonus;
+
+            var expect = 60000;
+            var actual = Employees.Find(ee => ee.Name == "CEO").Salary; 
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
