cdc7ae326810c806da3c6f1fa1f770fb548d4ec9
yuhanglin <yuhanglin@moneydj.com>
2020-03-25 09:11:19 +0800

    item 08 slide and project

---
+++ more-effective-csharp-2nd-edition/sources/item08/Item08.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace item08
+{
+    public class CTest
+    {
+        public T Transfrom<T>(T element, Func<T, T> transformFunc)
+        {
+            return transformFunc(element);
+        }
+    }
+
+    public class CTest2
+    {
+        public (T sought,int index) Find<T>(IEnumerable<T> enumerable,T value) //以tuple作為回傳值
+        {
+            int index = 0;
+            foreach(T element in enumerable)
+            {
+                if(element.Equals(value))
+                {
+                    return (value, index);
+                }
+                index++;
+            }
+            return (default(T), -1);
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item08.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using item08;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass]
+    public class Item08
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            CTest a = new CTest();
+            var aPoint = new { x = 5, y = 67 };
+            var bPoint = a.Transfrom(aPoint, (p) => new { x = p.x * 2, y = p.y * 2 });
+
+            Assert.AreSame(aPoint.GetType(), bPoint.GetType());
+            Assert.AreEqual(aPoint.x * 2, bPoint.x);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            CTest2 c = new CTest2();
+            List<int> _list = new List<int>{ 10, 20, 30 };
+            var a = c.Find(_list, 20);
+            (int FindValue, int FindIndex) b = c.Find(_list, 30);
+
+            Assert.AreEqual(a.index, 1);
+            Assert.AreEqual(b.FindIndex, 2);
+        }
+    }
+}
