215b30bd56a708c00d767f6010b866f4b88cb646
paigekuo <paigekuo@moneydj.com>
2020-04-29 15:15:38 +0800

    item 18 補充書裡範例

---
+++ more-effective-csharp-2nd-edition/sources/item18_2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace item18_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            C.Background =Brushes.Green;
+            base.OnMouseDown(e);
+        }
+
+        private void OnMouseDownHandler(object sender, MouseButtonEventArgs e)
+        {
+            C2.Background = Brushes.Red;
+        }
+    }
+}
