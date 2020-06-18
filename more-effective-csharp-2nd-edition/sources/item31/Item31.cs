b23bb2c02a82058dd2e4764459f359d50f4a8326
yuhanglin <yuhanglin@moneydj.com>
2020-06-18 08:34:04 +0800

    item 31 slides and projects

---
+++ more-effective-csharp-2nd-edition/sources/item31/Item31.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace item31
+{
+    public class Item31
+    {
+        public static async Task<int> DoAsyncThings()
+        {
+            await Task.Delay(1000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static async Task<int> DoAsyncThings2()
+        {
+            await Task.Delay(1000);
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public string msg = "default";
+        public async Task<Tuple<int, int>> SomeMethodAsync()
+        {
+            var result= await DoAsyncThings().ConfigureAwait(continueOnCapturedContext: false);
+            var result2 = await DoAsyncThings2().ConfigureAwait(continueOnCapturedContext: false);
+            return Tuple.Create(result, result2);
+        }
+
+        public async Task<int> AwareAsync()
+        {
+            //var tids = await SomeMethodAsync();
+            await AwareAsync2();
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public async Task AwareAsync2()
+        {
+            Task.Delay(100);
+        }
+    }
+
+    
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item31.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using item31;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class Item31
+    {
+        /*[TestMethod()]
+        public async Task TestSomeMethodAsync()
+        {
+            var target = new item31.Item31();
+            //var task = target.SomeMethodAsync();
+            //var expect = "In SomeMethodAsync,before the await";
+            //Assert.AreEqual(expect, target.msg);
+            //task.Wait();
+            //expect = "In SomeMethodAsync,after the await2";
+            //Assert.AreEqual(expect, target.msg);
+            var tids = await target.SomeMethodAsync();
+            Assert.AreEqual(tids.Item1, tids.Item2);
+        }*/
+
+        [TestMethod()]
+        public async Task TestAwareAsync()
+        {
+            var target = new item31.Item31();
+
+            var StratThreadID = Thread.CurrentThread.ManagedThreadId;
+            var AwareThreadID = await target.AwareAsync();
+            var EndThreadID = Thread.CurrentThread.ManagedThreadId;
+            Assert.AreEqual(StratThreadID, AwareThreadID);
+            Assert.AreEqual(StratThreadID, EndThreadID);
+        }
+    }
+}
