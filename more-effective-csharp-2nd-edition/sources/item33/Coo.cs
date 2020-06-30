ee056ad11eec671a642ac093cfa308bd21ebe046
cheshamchou <cheshamchou@moneydj.com>
2020-07-01 01:50:47 +0800

    item 33 slide and source

---
+++ more-effective-csharp-2nd-edition/sources/item33/Coo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace item33
+{
+    public class Coo
+    {
+        Random rnd { get; set; } = new Random();
+
+        public (int min, int max) randomBoundaries { get; set; } = (0, 0);
+
+        TimeSpan getRandomDelay() => TimeSpan.FromMilliseconds(rnd.Next(randomBoundaries.min, randomBoundaries.max));
+
+        public async Task foo(CancellationToken cancelToken, IProgress<(int, string)> progress)
+        {
+            // stage 1
+            cancelToken.ThrowIfCancellationRequested();
+            progress?.Report((0, "stage 1"));
+            await Task.Delay(getRandomDelay());
+            // stage 2
+            cancelToken.ThrowIfCancellationRequested();
+            progress?.Report((20, "stage 2"));
+            await Task.Delay(getRandomDelay());
+            // stage 3
+            cancelToken.ThrowIfCancellationRequested();
+            progress?.Report((40, "stage 3"));
+            await Task.Delay(getRandomDelay());
+            // stage 4
+            cancelToken.ThrowIfCancellationRequested();
+            progress?.Report((60, "stage 4"));
+            await Task.Delay(getRandomDelay());
+            // stage 5
+            progress?.Report((80, "stage 5"));
+            await Task.Delay(getRandomDelay());
+            progress?.Report((100, "done"));
+        }
+
+        public async Task foo() => await foo(CancellationToken.None, null);
+
+        public async Task foo(CancellationToken cancelToken) => await foo(cancelToken, null);
+
+        public async Task foo(IProgress<(int, string)> progress) => await foo(CancellationToken.None, progress);
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/item33.cs
@@ -0,0 +1,94 @@
+﻿using item33;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass]
+    public class item33
+    {
+        class ProgressReporter : IProgress<(int, string)>
+        {
+            public event EventHandler<(int, string)> onProgress;
+
+            public void Report((int, string) value)
+            {
+                onProgress?.Invoke(this, value);
+            }
+        }
+
+        [TestMethod]
+        public async Task 僅執行工作()
+        {
+            var target = new Coo();
+            await target.foo();
+        }
+
+        [TestMethod]
+        public async Task 不取消直接走完()
+        {
+            var progresses = new List<(int, string)>();
+            var reporter = new ProgressReporter();
+            reporter.onProgress += (sender, args) =>
+            {
+                progresses.Add(args);
+            };
+            var target = new Coo();
+            await target.foo(reporter);
+            Assert.AreEqual(6, progresses.Count);
+            Assert.IsTrue(Enumerable.Range(0, 6).Select(i => i * 20).SequenceEqual(progresses.Select(i => i.Item1)));
+        }
+
+        [TestMethod]
+        public async Task 取消不走完()
+        {
+            var progresses = new List<(int, string)>();
+            var timeout = TimeSpan.FromMilliseconds(50);
+            using (var cts = new CancellationTokenSource(timeout))
+            {
+                var reporter = new ProgressReporter();
+                reporter.onProgress += (sender, args) =>
+                {
+                    progresses.Add(args);
+                };
+                var target = new Coo { randomBoundaries = (0, (int)timeout.TotalMilliseconds * 2) };
+                try
+                {
+                    await target.foo(cts.Token, reporter);
+                }
+                catch (OperationCanceledException ce)
+                {
+                    Logger.LogMessage("{0}\n{1}", ce.Message, ce.StackTrace);
+                }
+            }
+            Assert.AreNotEqual(6, progresses.Count);
+            Assert.IsTrue(Enumerable.Range(0, progresses.Count).Select(i => i * 20).SequenceEqual(progresses.Select(i => i.Item1)));
+        }
+
+        [TestMethod]
+        public async Task 取消且不回報()
+        {
+            var timeout = TimeSpan.FromMilliseconds(50);
+            var caughtException = default(OperationCanceledException);
+            using (var cts = new CancellationTokenSource(timeout))
+            {
+                var target = new Coo { randomBoundaries = (0, (int)timeout.TotalMilliseconds * 2) };
+                try
+                {
+                    await target.foo(cts.Token);
+                }
+                catch (OperationCanceledException ce)
+                {
+                    Logger.LogMessage("{0}\n{1}", ce.Message, ce.StackTrace);
+                    caughtException = ce;
+                }
+            }
+            Assert.IsNotNull(caughtException);
+        }
+    }
+}
