14793c16b81ff70d3ec6c9138e00a09c59252c3c
cheshamchou <cheshamchou@moneydj.com>
2020-07-01 08:54:42 +0800

    item 34 slide and source

---
+++ more-effective-csharp-2nd-edition/sources/item34/Coo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace item34
+{
+    public class Coo
+    {
+        public TimeSpan cacheTimeout { get; set; } = TimeSpan.Zero;
+
+        public Func<DateTime> getNow { get; set; } = () => DateTime.Now;
+
+        public (int min, int max) randomRange { get; set; } = (0, 0);
+
+        Random rnd { get; set; } = new Random();
+
+        DateTime? lastCacheTime { get; set; }
+
+        int? data { get; set; }
+
+        public ValueTask<int> getCache()
+        {
+            try
+            {
+                if (!lastCacheTime.HasValue || (getNow() - lastCacheTime) > cacheTimeout)
+                {
+                    async Task<int> query()
+                    {
+                        var value = rnd.Next(randomRange.min, randomRange.max);
+                        await Task.Delay(TimeSpan.FromMilliseconds(value));
+                        data = value;
+                        return value;
+                    }
+                    return new ValueTask<int>(query());
+                }
+                else
+                {
+                    return new ValueTask<int>(data.Value);
+                }
+            }
+            finally
+            {
+                lastCacheTime = DateTime.Now;
+            }
+        }
+
+        public async Task<int> getCacheTask()
+        {
+            try
+            {
+                if (!lastCacheTime.HasValue || (getNow() - lastCacheTime) > cacheTimeout)
+                {
+                    var value = rnd.Next(randomRange.min, randomRange.max);
+                    await Task.Delay(TimeSpan.FromMilliseconds(value));
+                    data = value;
+                    return value;
+                }
+                else
+                {
+                    return data.Value;
+                }
+            }
+            finally
+            {
+                lastCacheTime = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/item34.cs
@@ -0,0 +1,55 @@
+﻿using item34;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass]
+    public class item34
+    {
+        [TestMethod]
+        public async Task 測試非同步取得快取結果()
+        {
+            var target = new Coo();
+            var r = await target.getCache();
+            Assert.AreEqual(0, r);
+        }
+
+        [TestMethod]
+        public async Task 測試緊湊迴圈使用緩衝擴充取得非同步快取結果()
+        {
+            var target = new Coo();
+            for (var i = 0; i < 1000; ++i)
+            {
+                var r = await target.getCache();
+                Assert.AreEqual(0, r);
+            }
+        }
+
+        [TestMethod]
+        public async Task 測試緊湊迴圈取得非同步快取結果()
+        {
+            var target = new Coo();
+            for (var i = 0; i < 1000; ++i)
+            {
+                var r = await target.getCacheTask();
+                Assert.AreEqual(0, r);
+            }
+        }
+
+        [TestMethod]
+        public async Task 快取逾時前會直接取用快取不重新評估()
+        {
+            var target = new Coo { cacheTimeout = TimeSpan.FromSeconds(1), randomRange = (50, 50) };
+            var r = await target.getCache();
+            Assert.AreEqual(50, r);
+            target.randomRange = (0, 0);
+            r = await target.getCache();
+            Assert.AreEqual(50, r);
+            target.getNow = () => DateTime.Now + TimeSpan.FromSeconds(2);
+            r = await target.getCache();
+            Assert.AreEqual(0, r);
+        }
+    }
+}
