9e1585fdd4770613fbb02792f931fba3b13b14e7
ryanliu <ryanliu@moneydj.com>
2020-07-15 14:58:09 +0800

    item 37、38 slide and source

---
+++ more-effective-csharp-2nd-edition/sources/item38/item38.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace item38
+{
+    public class item38
+    {
+
+
+    }
+
+    public class WorkEngine
+    {
+        public virtual event EventHandler<DoWorkEventArgs> handler;
+        private BackgroundWorker _demoBGWorker = new BackgroundWorker();
+        public void Start()
+        {
+            _demoBGWorker.DoWork += BGWorker_DoWork;
+            _demoBGWorker.RunWorkerAsync();
+        }
+
+        public void Start(int EndNmumber)
+        {
+            _demoBGWorker.DoWork += BGWorker_DoWork;
+            _demoBGWorker.WorkerSupportsCancellation = true;
+            _demoBGWorker.RunWorkerAsync(EndNmumber);
+        }
+
+        public void Cancel()
+        {
+            _demoBGWorker.CancelAsync();
+        }
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = sender as BackgroundWorker;
+
+            if (bgWorker.CancellationPending == true)
+            {
+                e.Cancel = true;
+                e.Result = -999;
+                handler?.Invoke(this, e);
+                return;
+            }
+            if (e.Argument == null || !int.TryParse(e.Argument.ToString(), out int endNumber))
+            {
+                endNumber = 100;
+            }
+
+            //在這裡執行耗時的運算。
+            int sum = 0;
+            for (int i = 0; i <= endNumber; i++)
+            {
+                sum += i;
+            }
+            e.Result = sum;
+        }
+
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/item38.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using item38;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class item38
+    {
+        [TestMethod()]
+        public void TestBackgroundWorkerGetResult()
+        {
+            var e = new EventHandler<DoWorkEventArgs>((sender, args) =>
+            {
+                Assert.AreEqual(55, args.Result);
+            });
+            WorkEngine engine = new WorkEngine();
+            engine.handler += e;
+            engine.Start(10);
+        }
+        [TestMethod()]
+        public void TestBackgroundWorkerCancel()
+        {
+            var e = new EventHandler<DoWorkEventArgs>((sender, args) =>
+            {
+                Assert.IsTrue(args.Cancel);
+                Assert.AreEqual(-999, args.Result);
+            });
+            WorkEngine engine = new WorkEngine();
+            engine.handler += e;
+            engine.Start(10);
+            engine.Cancel();
+
+        }
+    }
+}
