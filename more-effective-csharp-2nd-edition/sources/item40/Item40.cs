6e36bceaf7dd456ef391c45c9d56f8c9ad85e20e
yuhanglin <yuhanglin@moneydj.com>
2020-07-23 10:32:09 +0800

    item 39、40 slide and source

---
+++ more-effective-csharp-2nd-edition/sources/item40/Item40.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace item40
+{
+    public class Item40
+    {
+        int total = 0;
+        object syncHandle = new object();
+
+        public int TotalValue
+        {
+            get
+            {
+                lock (syncHandle)
+                { return total; }
+            }
+        }
+
+        public int AddTotal_Lock()
+        {
+            lock (syncHandle)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    total += i;
+
+                    if (i == 1)
+                    {
+                        Thread t1 = new Thread(ChangeTotal);
+                        t1.Start(100);
+                    }
+                }
+            }
+            Thread.Sleep(100);
+            return total;
+        }
+
+        public int AddTotal_NoLock()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                total += i;
+
+                if (i == 1)
+                {
+                    Thread t1 = new Thread(ChangeTotal);
+                    t1.Start(100);
+                }
+                Thread.Sleep(100);
+            }
+            return total;
+        }
+
+        public void ChangeTotal(object value)
+        {
+            lock (syncHandle)
+            {
+                total = (int)value;
+            }
+        }
+
+        //------------------------------------------------
+
+        int Value = 0;
+        //int syncHandle2 = 1;
+
+        public int GetValue
+        {
+            get
+            {
+                return Value;
+            }
+        }
+
+        public int ChangeValue1()
+        {
+            Monitor.Enter(syncHandle);
+            for (int i = 1; i <= 10; i++)
+            {
+                Value += i;
+
+                if (i == 1)
+                {
+                    Thread t1 = new Thread(ChangeValue2);
+                    t1.Start(100);
+                }
+            }
+            Monitor.Exit(syncHandle);
+            Thread.Sleep(100);
+            return Value;
+        }
+
+        public void ChangeValue2(object value)
+        {
+            Monitor.Enter(syncHandle);
+            Value = (int)value;
+            Monitor.Exit(syncHandle);
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item40.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using item40;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class Item40
+    {
+        [TestMethod()]
+        public void NoLock()
+        {
+            item40.Item40 a = new item40.Item40();
+            Assert.AreEqual(0, a.TotalValue);
+            a.AddTotal_NoLock();
+            Assert.AreNotEqual(55, a.TotalValue);
+        }
+
+        [TestMethod()]
+        public void Lock()
+        {
+            item40.Item40 a = new item40.Item40();
+            Assert.AreEqual(0, a.TotalValue);
+            a.AddTotal_Lock();
+            Assert.AreEqual(100, a.TotalValue);
+        }
+
+        [TestMethod()]
+        public void MonitorLock()
+        {
+            item40.Item40 a = new item40.Item40();
+            Assert.AreEqual(0, a.GetValue);
+            a.ChangeValue1();
+            Assert.AreEqual(100, a.GetValue);
+        }
+    }
+}
