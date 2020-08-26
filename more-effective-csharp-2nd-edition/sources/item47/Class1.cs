3928d81eb41127afa91ec38c346f553991cca321
yuhanglin <yuhanglin@moneydj.com>
2020-08-26 13:40:01 +0800

    item 47, 48 slides and sources

---
+++ more-effective-csharp-2nd-edition/sources/item47/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace item47
+{
+    public class Class1
+    {
+        private static dynamic DynamicAdd(dynamic left, dynamic right)
+        {
+            return left + right;
+        }
+
+        public static T1 Add<T1 , T2>(T1 left, T2 right)
+        {
+            dynamic result = DynamicAdd(left, right);
+            return (T1)result;
+        }
+    }
+}
---
+++ more-effective-csharp-2nd-edition/sources/more-effective-csharp-2nd-edition/Item47.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using item47;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace more_effective_csharp_2nd_edition
+{
+    [TestClass()]
+    public class Item47
+    {
+        [TestMethod()]
+        public void Add()
+        {
+            int answer = item47.Class1.Add(5, 4);
+            Assert.AreEqual(9, answer);
+        }
+    }
+}
